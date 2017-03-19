f53ef2222a7bc0358348871ed94c272b59cc34cf
Chris Dorst <chris@dorst.email>
2017-03-19 06:34:00 -0500

    Adding IFunction

---
+++ Automation.Functions.Abstractions/IFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Automation.Functions.Abstractions
+{
+    /// <summary>
+    /// Contains a function that takes a state object as its first
+    /// argument, any configuration relevant to the function as its second
+    /// argument, and returns the state object
+    /// </summary>
+    public interface IFunction
+    {
+        /// <summary>
+        /// Contains a function that takes a state object as its first
+        /// argument, any configuration relevant to the function as its second
+        /// argument, and returns the state object
+        /// </summary>
+        Func<dynamic, dynamic, dynamic> Function { get; }
+    }
+}
